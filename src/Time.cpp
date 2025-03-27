c7179bfdf449d92a885f186dbf4cea7abf9016e2
ItzDJYP <130939251+ItzDJYP@users.noreply.github.com>
2025-03-27 15:32:01 +0000

    lab 5 submission

--- src/Time.cpp
+++ src/Time.cpp
@@ -3,31 +3,38 @@
 using namespace std;
 
 // Overload the + operator
-Time Time::operator+(const Time &other) const
-{   
+Time Time::operator+(const Time &other) const{
+    int totalMinutes = minutes + other.minutes;
+    int totalHours = hours + other.hours + totalMinutes / 60;
+    totalMinutes %= 60;
+    return Time(totalHours, totalMinutes);
 }
 
 // Overload the += operator
-Time& Time::operator+=(const Time &other)
-{   
+Time& Time::operator+=(const Time &other){
+    minutes += other.minutes;
+    hours += other.hours + minutes / 60;
+    minutes %= 60;
+    return *this;
 }
 
 // Overload the < operator
-bool Time::operator<(const Time &other) const
-{
+bool Time::operator<(const Time &other) const{
+    return (hours * 60 + minutes) < (other.hours * 60 + other.minutes);
 }
 
 // Overload the > operator
-bool Time::operator>(const Time &other) const
-{
+bool Time::operator>(const Time &other) const{
+    return (hours * 60 + minutes) > (other.hours * 60 + other.minutes);
 }
 
 // Overload the == operator
-bool Time::operator==(const Time &other) const
-{
+bool Time::operator==(const Time &other) const{
+    return (hours * 60 + minutes) == (other.hours * 60 + other.minutes);
 }
 
 // Overload the friend function operator<<
-ostream &operator<<(ostream &os, const Time &tm)
-{
+ostream &operator<<(ostream &os, const Time &tm){
+    os << tm.getHours() << ":" << (tm.getMinutes() < 10 ? "0" : "") << tm.getMinutes();
+    return os;
 }
